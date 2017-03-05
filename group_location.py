f1b1dc9e43c78b7d52016b517de6a632518877f3
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2017-03-04 21:01:32 -0500

    Fix for partially recognized country

--- group_location.py
+++ group_location.py
@@ -12,6 +12,8 @@ def get_country_from_location(lat, lon):
     geolocator = Nominatim()
     location = geolocator.reverse(str(lat)+', '+str(lon))
     code = location.raw[u'address'][u'country_code'].encode('utf-8')
+    if code == 'xk':
+        return 'Kosovo'
     country = countries.get(code)
     return country.name
 
