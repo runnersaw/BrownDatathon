0cfececa38235223a180e177deffb9894ec8e2c1
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2017-03-04 22:49:26 -0500

    Use 3-letter country codes

--- group_location.py
+++ group_location.py
@@ -2,6 +2,9 @@
 from geopy.geocoders import Nominatim
 from iso3166 import countries
 
+def country_number_to_code(number):
+    return countries.get(number).alpha3
+
 def get_group_location(d, group_name):
     group = d.loc[d['gname'] == group_name]
     lat = group['latitude'].median()
@@ -14,10 +17,10 @@ def get_country_from_location(lat, lon):
     try:
         code = location.raw[u'address'][u'country_code'].encode('utf-8')
     except:
-        print('Failed to get address for '+str(lat)+','str(lon)+' with dict: '+location.raw)
+        print('Failed to get address for '+str(lat)+','+str(lon)+' with dict: '+location.raw)
         return None
     if code == 'xk':
-        return 'Kosovo'
+        return 'UNK'
     country = countries.get(code)
     return country.name
 
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from database import data
 from group_location import *
 
 if __name__=="__main__":
+	print(country_number_to_code('united states'))
 	lat, lon = get_group_location(data, 'Abkhazian Separatists')
 	print(lat, lon)
 	print(get_country_from_location(lat, lon))
