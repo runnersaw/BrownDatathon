90bf1460ec857015c138c4fb6538b9b74379da7d
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2017-03-04 20:50:01 -0500

    Update country lookup

--- group_location.py
+++ group_location.py
@@ -1,6 +1,6 @@
 
-from urllib2 import urlopen
-import json
+from geopy.geocoders import Nominatim
+from iso3166 import countries
 
 def get_group_location(d, group_name):
     group = d.loc[d['gname'] == group_name]
@@ -9,14 +9,9 @@ def get_group_location(d, group_name):
     return (lat, lon)
 
 def get_country_from_location(lat, lon):
-    url = "http://maps.googleapis.com/maps/api/geocode/json?"
-    url += "latlng=%s,%s&sensor=false" % (lat, lon)
-    v = urlopen(url).read()
-    j = json.loads(v)
-    components = j['results'][0]['address_components']
-    country = None
-    for c in components:
-        if "country" in c['types']:
-            country = c['long_name']
-    return country
+    geolocator = Nominatim()
+    location = geolocator.reverse(str(lat)+', '+str(lon))
+    code = location.raw[u'address'][u'country_code'].encode('utf-8')
+    country = countries.get(code)
+    return country.name
 
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from database import data
 from group_location import *
 
 if __name__=="__main__":
-	lat, lon = get_group_location(data, 'Taliban')
+	lat, lon = get_group_location(data, 'Abkhazian Separatists')
+	print(lat, lon)
 	print(get_country_from_location(lat, lon))
 
